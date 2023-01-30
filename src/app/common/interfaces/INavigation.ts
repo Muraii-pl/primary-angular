d2bd2b1ec8bbd3134b0f4e100f1ba206464967de
Mateusz Glowania <mglowani@createctrl.com>
2023-01-30 13:41:23 +0100

    Add footer / navigation / add tabindex

--- src/app/common/interfaces/INavigation.ts
+++
@@ -1,4 +0,0 @@
-export interface INavigation {
-  title: string;
-  slug: string;
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'https://muraii-test.cba.pl/wp-json/wp/v2'
-};
